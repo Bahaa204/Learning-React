35b58e62796fe6586706bd8ee349931383203f35
Bahaa204 <brawass6@gmail.com>
2026-01-30 18:35:12 +0200

    Finished the contact form

--- Project-3/src/App.jsx
+++ Project-3/src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home";
 import About from "./Pages/About";
+import Contact from "./Pages/Contact";
 import "./App.css";
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route index element={<Home />} />
         <Route path="about" element={<About />} />
+        <Route path="contact" element={<Contact />} />
       </Routes>
     </>
   );
