5a16afbd8722c0513585b3c5192ad3c7102f90ab
Bahaa204 <brawass6@gmail.com>
2026-01-30 17:43:31 +0200

    changing the logo in the header

--- Project-3/src/components/Header.jsx
+++ Project-3/src/components/Header.jsx
@@ -1,11 +1,12 @@
 import NavBar from "./NavBar";
-import "../../assets/CSS/Header.css"
+import Logo from "../../assets/Images/Header-logo-pixelated.png";
+import "../../assets/CSS/Header.css";
 
 export default function Header() {
   return (
     <header className="navbar">
-      <a href="/CookLab/index.html" className="logo">
-        🍳 CookLab
+      <a href="/" className="logo">
+        <img src={Logo} alt="logo" />
       </a>
       <NavBar />
     </header>
