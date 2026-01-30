72b52e2625a94683d89594b20713a730724342f9
Bahaa204 <brawass6@gmail.com>
2026-01-30 12:48:19 +0200

    Refactored Favorites

--- Project-2/src/Pages/Favorites.jsx
+++ Project-2/src/Pages/Favorites.jsx
@@ -6,26 +6,23 @@ import { useMovieContext } from "../contexts/Contexts";
 export default function Favorites() {
   const { Favorites } = useMovieContext();
 
-  if (Favorites.length >= 1) {
-    return (
-      <>
-        <NavBar />
+  return (
+    <>
+      <NavBar />
+      {Favorites.length >= 1 ? (
         <div className="MovieGrid">
           {Favorites.map((movie) => (
             <MovieCard movie={movie} key={movie.id} />
           ))}
         </div>
-      </>
-    );
-  }
-
-  return (
-    <>
-      <NavBar />
-      <div className="favorites-empty">
-        <h2>No Favorite Movies Yet</h2>
-        <p>Start adding movies to your favorites and they will appear here!</p>
-      </div>
+      ) : (
+        <div className="favorites-empty">
+          <h2>No Favorite Movies Yet</h2>
+          <p>
+            Start adding movies to your favorites and they will appear here!
+          </p>
+        </div>
+      )}
     </>
   );
 }
